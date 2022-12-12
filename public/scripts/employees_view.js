a3b9d6e82c76c1239ba58beada5711fccc49cca8
mepperson3 <mepperson3@gatech.edu>
2022-12-12 15:48:41 -0500

    Added remove_pilot_role(), add_pilot_role(), add_worker_role() to employees_view.html

--- public/scripts/employees_view.js
+++ public/scripts/employees_view.js
@@ -7,16 +7,27 @@ function displaySelect() {
 }
 
 let employeeData = null;
+let workerData = null;
+let pilotData = null;
 
 function displaySelectHandler() {
     if (this.response.success) {
         console.log("SUCCESSSS");
-        if (this.response.data) {
-            let data = this.response.data;
-            employeeData = data;
+        if (this.response.employees) {
+            employeeData = this.response.employees
+            workerData = this.response.workers
+            pilotData = this.response.pilots
             // document.getElementById("select_output").innerText = JSON.stringify(this.response.data)     }
-            for (let i = 0; i < data.length; i++) {
-                populateTable(data[i]);
+            for (let i = 0; i < employeeData.length; i++) {
+                populateTable(employeeData[i], "employeeTable");
+            }
+
+            for (let i = 0; i < workerData.length; i++) {
+                populateTable(workerData[i], "workerTable")
+            }
+
+            for (let i = 0; i < pilotData.length; i++) {
+                populateTable(pilotData[i], "pilotTable")
             }
         }
     } else {
@@ -34,21 +45,21 @@ function filterTable() {
             console.log(employee);
             if (username != "" && taxID == "") {
                 if (employee.username == username) {
-                    clearTable();
+                    clearTable("employeeTable");
                     populateTable(employee);
                     break;
                 }
             }
             if (taxID != "" && username == "") {
                 if (employee.taxID == taxID) {
-                    clearTable();
+                    clearTable("employeeTable");
                     populateTable(employee);
                     break;
                 }
             }
             if (taxID != "" && username != "") {
                 if (employee.taxID == taxID && employee.username == username) {
-                    clearTable();
+                    clearTable("employeeTable");
                     populateTable(employee);
                     break;
                 }
@@ -60,8 +71,8 @@ function filterTable() {
 }
 
 
-function clearTable() {
-    const table = document.getElementById("employeeTable");
+function clearTable(tableName) {
+    const table = document.getElementById(tableName);
 
     for (let i = table.rows.length - 1; i > 0; i--) {
         table.deleteRow(i);
@@ -70,21 +81,125 @@ function clearTable() {
 
 
 
-function populateTable(items) {
-    const table = document.getElementById("employeeTable");
+function populateTable(items, tableName) {
+    const table = document.getElementById(tableName);
 
     let itemKeys = Object.keys(items);
     
     let row = table.insertRow();
-    for (let i = 0; i < itemKeys.length; i++) {
-        let colName = row.insertCell(i);
-        let attribute = itemKeys[i];
-        if (attribute == "hired") {
-            colName.innerHTML = items[attribute].substring(0, 10);
-            continue;
+    if (tableName == 'employeeTable') {
+        for (let i = 0; i < itemKeys.length; i++) {
+            let colName = row.insertCell(i);
+            let attribute = itemKeys[i];
+            if (attribute == "hired") {
+                colName.innerHTML = items[attribute].substring(0, 10);
+                continue;
+            }
+            colName.innerHTML = items[attribute];
         }
-        colName.innerHTML = items[attribute];
+    } else {
+        for (let i = 0; i < itemKeys.length; i++) {
+            let colName = row.insertCell(i);
+            let attribute = itemKeys[i];
+            colName.innerHTML = items[attribute];
+        }
     }
+}
+
+document.getElementById("addPilotRole").onclick = function() {
+    let username = document.getElementById("username")
+    let licenseID = document.getElementById("inputLicenseID")
+    let experience = document.getElementById("inputExperience")
+
+    addPilotRole(username.value, licenseID.value, experience.value)
+    clearTable("pilotTable")
+    clearTable("workerTable")
+    displaySelect()
+}
+
+
+function addPilotRole(username, licenseID, experience) {
+    let information = `username=${username}&licenseID=${licenseID}&experience=${experience}`
+    addPilotRoleRequest(information)
+}
+
+
+function addPilotRoleRequest(information) {
+    let xml = new XMLHttpRequest
+    xml.responseType = "json"
+    xml.addEventListener("load", addPilotRoleResponse)
+    url = "/attempt-add-pilot-role"
+    
+    xml.open("POST", url)
+    xml.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+    xml.send(information)
+}
+
+
+function addPilotRoleResponse() {
+
+}
+
+
+function removePilotRole(username) {
+    let information = `username=${username}`
+    removePilotRoleRequest(information)
+}
+
+function removePilotRoleRequest(information) {
+    let xml = new XMLHttpRequest
+    xml.responseType = "json"
+    xml.addEventListener("load", removePilotRoleResponse)
+    url = "/attempt-remove-pilot-role"
+    
+    xml.open("POST", url)
+    xml.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+    xml.send(information)
+}
+
+function removePilotRoleResponse() {
+
+}
+
+
+document.getElementById("removePilotRole").onclick = function() {
+    let username = document.getElementById("username")
+
+    removePilotRole(username.value)
+    clearTable("pilotTable")
+    clearTable("workerTable")
+    displaySelect()
+}
+
+
+document.getElementById("addWorkerRole").onclick = function() {
+    let username = document.getElementById("username")
+
+    addWorkerRole(username.value)
+    clearTable("workerTable")
+    clearTable("pilotTable")
+    displaySelect()
+}
+
+
+function addWorkerRole(username) {
+    let information = `username=${username}`
+    addWorkerRoleRequest(information)
+}
+
+
+function addWorkerRoleRequest(information) {
+    let xml = new XMLHttpRequest
+    xml.responseType = "json"
+    xml.addEventListener("load", addWorkerRoleResponse)
+    url = "/attempt-add-worker-role"
+    
+    xml.open("POST", url)
+    xml.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+    xml.send(information)
+}
+
+function addWorkerRoleResponse(information) {
 }
 
 
@@ -99,7 +214,12 @@ document.getElementById('taxID').onchange = function() {
 document.getElementById('clearBtn').onclick = function() {
     document.getElementById('username').value = "";
     document.getElementById('taxID').value = "";
-    clearTable();
+    document.getElementById('inputLicenseID').value = "";
+    document.getElementById('inputExperience').value = "";
+    clearTable("employeeTable");
+    clearTable("workerTable")
+    clearTable("pilotTable")
+    displaySelect()
     filterTable();
 };
 
@@ -107,4 +227,4 @@ document.addEventListener("DOMContentLoaded", function() {
     displaySelect();
   });
 
-document.getElementById("query_input").addEventListener("click", displaySelect);
+// document.getElementById("query_input").addEventListener("click", displaySelect);
--- server.js
+++ server.js
@@ -281,6 +281,56 @@ app.post("/attempt-remove-ingredient", function(req, res) {
 })
 
 
+app.post("/attempt-add-pilot-role", function(req, res) {
+    let pilotRole = 'call add_pilot_role(?,?,?)'
+    connection.query(pilotRole, [
+        req.body.username,
+        req.body.licenseID,
+        req.body.experience
+    ], function(err, rows){
+        if (err) {
+            res.json({success: false, message: "database query failed for /attempt-add-pilot-role"})
+            console.log(err.message)
+        } else {
+            res.json({success: true, message: "successfully addd pilot"})
+            console.log("pilot was added")
+        }
+    })
+})
+
+
+app.post("/attempt-add-worker-role", function(req, res) {
+    let workerRole = 'call add_worker_role(?)'
+    connection.query(workerRole, [
+        req.body.username
+    ], function(err, rows){
+        if (err) {
+            res.json({success: false, message: "database query failed for /attempt-add-worker-role"})
+            console.log(err.message)
+        } else {
+            res.json({success: true, message: "successfully add worker role"})
+            console.log("worker role was added")
+        }
+    })
+})
+
+
+app.post("/attempt-remove-pilot-role", function(req, res) {
+    let pilotRole = 'call remove_pilot_role(?)'
+    connection.query(pilotRole, [
+        req.body.username
+    ], function(err, rows){
+        if (err) {
+            res.json({success: false, message: "database query failed for /attempt-remove-pilot-role"})
+            console.log(err.message)
+        } else {
+            res.json({success: true, message: "successfully removed pilot"})
+            console.log("pilot was removed")
+        }
+    })
+})
+
+
 app.post("/attempt-purchase-ingredient", function(req, res) {
     let purchaseIngredient = 'call purchase_ingredient(?, ?, ?, ?, ?)'
     connection.query(purchaseIngredient, [
@@ -427,13 +477,38 @@ app.get("/display-locations-view", function(req, res) {
 
 
 app.get("/display-employee-view", function(req, res) {
-    userQuery = "select * from display_employee_view"
-    connection.query(userQuery, function(err, rows) {
+    employeeQuery = "select * from display_employee_view"
+    workerQuery = "select * from workers"
+    pilotQuery = "select * from pilots"
+
+    let employees;
+    connection.query(employeeQuery, function(err, rows) {
         if (err) {
             res.json({success: false, message:"database query failed for /display-select"})
         } else {
-            console.log(userQuery);
-            res.json({success: true, data: rows})
+            console.log(employeeQuery);
+            employees = rows
+        }
+    })
+
+    let workers;
+    connection.query(workerQuery, function(err, rows) {
+        if (err) {
+            res.json({success: false, message:"database query failed for /display-select"})
+        } else {
+            console.log(workerQuery);
+            workers = rows
+        }
+    })
+
+    let pilots;
+    connection.query(pilotQuery, function(err, rows) {
+        if (err) {
+            res.json({success: false, message:"database query failed for /display-select"})
+        } else {
+            console.log(pilotQuery);
+            pilots = rows
+            res.json({success: true, employees: employees, workers: workers, pilots: pilots})
         }
     })
 })
@@ -486,6 +561,7 @@ app.get("/display-owners-view", function(req, res) {
         }
     })
 })
+
 
 
 app.get("/display-drones", function(req, res) {

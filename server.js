90162729ef2f1445e7d2b02481dbd99534fdebbe
mepperson3 <mepperson3@gatech.edu>
2022-12-15 14:56:14 -0500

    Changed 'drone management' to 'Pilot Dashboard' on navbar

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const connection = mysql.createConnection({
 
     host: "localhost",
     user: "root",
-    password: "password",
+    password: "asdqwe123",
     database: "restaurant_supply_express"
 
 })
